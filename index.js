d1393de622bebc8599be4cdad0ba9227d5c9d787
Alexander Strasser <alexander.strasser@protonmail.com>
2025-01-26 10:23:31 +0100

    temp local image call in index

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import { getEmbedding } from "./embed.js";
+import { getLocalEmbedding } from "./embedLocal.js";
 import { storeEmbedding, retrieveEmbedding, clearEmbeddings } from "./db.js";
 
 const embeddingMap = {};
 
 const API_ENDPOINT = "https://api.edenai.run/v2/image/embeddings";
 const FILE_URL = "https://markusstrasser.org/images/fineart_collage2.jpg";
-
+const LOCAL_IMAGE_PATH = "/Users/alien/Projects/embedviz/datasets/ikarus777/best-artworks-of-all-time/versions/1/images/images/Albrecht_Du╠êrer/Albrecht_Du╠êrer_1.jpg";
 async function main() {
   const apiKey = process.env.EDEN_API_KEY;
   if (!apiKey) {
@@ -15,7 +16,7 @@ async function main() {
 
   try {
     clearEmbeddings();
-    const embedding = await getEmbedding(FILE_URL, apiKey, API_ENDPOINT);
+    const embedding = await getLocalEmbedding(LOCAL_IMAGE_PATH, apiKey, API_ENDPOINT);
     console.log("Embedding:", embedding);
     const id = storeEmbedding(embedding);
     console.log("Stored embedding with id:", id);
