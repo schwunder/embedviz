8c6ba71353259b552432b4c713077657a962ce31
Alexander Strasser <alexander.strasser@protonmail.com>
2025-01-29 12:35:33 +0100

    pca error logging and batch function

--- pca.js
+++ pca.js
@@ -1,14 +1,39 @@
 import { PCA } from "ml-pca";
 
-export const dimensionalityReduction = (embeddings) => {
-  // Create PCA object
-  console.log("embeddings", embeddings);
-  const pca = new PCA(embeddings);
-  console.log("pca", pca);
+export const dimensionalityReduction = (embeddings, pcaObject = null) => {
+  // Validate embeddings
+  if (!Array.isArray(embeddings) || embeddings.length === 0) {
+    throw new TypeError("Embeddings must be a non-empty array.");
+  }
+  if (embeddings.some(embedding => !(embedding instanceof Float32Array))) {
+    throw new TypeError("Embeddings must be an array of Float32Array.");
+  }
+
+  // Use existing PCA object or create new one
+  const pca = pcaObject || new PCA();
   const projection = pca.predict(embeddings, { nComponents: 2 });
 
   // Convert Matrix to number[][]
   const projectionArray = projection.to2DArray();
-  console.log("projectionArray", projectionArray);
   return projectionArray;
+};
+
+export const dimensionalityReductionBatch = (embeddingsWithIdArray) => {
+  // Validate embeddingsWithIdArray
+  if (!Array.isArray(embeddingsWithIdArray) || embeddingsWithIdArray.length === 0) {
+    throw new TypeError("EmbeddingsWithIdArray must be a non-empty array.");
+  }
+  if (embeddingsWithIdArray.some(embedding => !("id" in embedding) || !("embedding" in embedding))) {
+    throw new TypeError("EmbeddingsWithIdArray must be an array of objects with 'id' and 'embedding' properties.");
+  }
+  if (embeddingsWithIdArray.some(embedding => !(embedding.embedding instanceof Uint8Array))) {
+    throw new TypeError("Embedding must be a Uint8Array.");
+  }
+  const pca = new PCA();
+  const projectionsWithIdArray = embeddingsWithIdArray.map(({ id, embedding }) => {
+    const floatEmbedding = new Float32Array(embedding);
+    const projection = dimensionalityReduction([floatEmbedding], pca)[0];
+    return { id, projection };
+  });
+  return projectionsWithIdArray;
 };
