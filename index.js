8d90342ec548abe612fa669d8c290e34e3edf612
Alexander Strasser <alexander.strasser@protonmail.com>
2025-01-29 13:13:10 +0100

    naming

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-import { getEmbedding } from "./embed.js";
-import { getLocalEmbedding, getBatchLocalEmbedding } from "./embedLocal.js";
-import { storeEmbedding, retrieveEmbedding, clearEmbeddings } from "./db.js";
-import { getFilesFromFolders, getFilesByArtist } from "./batchExecution.js";
-
+import { getEmbeddingBatch } from "./embed.js";
+import { 
+  deleteAllEmbeddings,
+  getAllEmbeddings,
+  storeEmbeddingBatch,
+  patchEmbeddingBatch
+    } from "./db.js";
+import { getFilesFromFolders } from "./paths.js";
+import { dimensionalityReductionBatch } from "./pca.js";
 
 const API_ENDPOINT = "https://api.edenai.run/v2/image/embeddings";
-const FILE_URL = "https://markusstrasser.org/images/fineart_collage2.jpg";
-const LOCAL_IMAGE_PATH = "/Users/alien/Projects/embedviz/datasets/ikarus777/best-artworks-of-all-time/versions/1/images/images/Albrecht_Du╠êrer/Albrecht_Du╠êrer_1.jpg";
+const IMAGE_PATH = 'datasets/ikarus777/best-artworks-of-all-time/versions/1/images/images';
 async function main() {
   const apiKey = process.env.EDEN_API_KEY;
-const retrievedPaths = getFilesFromFolders(2, 1, 'datasets/ikarus777/best-artworks-of-all-time/versions/1/images/images/');
-console.log('Retrieved paths from folders:', retrievedPaths);
-  if (!apiKey) {
-    console.error("Please set EDEN_API_KEY environment variable");
-    process.exit(1);
-  }
 
-  try {
-    clearEmbeddings();
-    const embeddings = await getBatchLocalEmbedding(retrievedPaths, apiKey, API_ENDPOINT);
-    for (const embedding of embeddings) {
-      const id = storeEmbedding(embedding);
-      console.log("Stored embedding with id:", id);
-      const retrievedEmbedding = retrieveEmbedding(id);
-      console.log("Retrieved embedding:", id);
-    }
-  } catch (error) {
-    console.error("Error getting embedding:", error);
-  }
+  const retrievedPaths = getFilesFromFolders(2, 1, IMAGE_PATH);
+  console.log('Retrieved paths from folders:', retrievedPaths);
+  
+  //deleteAllEmbeddings (); 
+  //console.log('Deleted all embeddings');
+  
+  //let embeddings = [];
+  //embeddings = await getEmbeddingBatch(retrievedPaths, apiKey, API_ENDPOINT);
+  //console.log("embeddings", embeddings[0][0]);
+  
+  //const storedIds = storeEmbeddingBatch(embeddings);
+  //console.log("stored ids", storedIds);
+  
+  const embeddingWithIdArray = getAllEmbeddings();
+  console.log("Retrieved embeddings:", embeddingWithIdArray[0]);
+  
+  
+  const projectionWithIdArray = dimensionalityReductionBatch(embeddingWithIdArray);
+  console.log("projection array", projectionWithIdArray[0]);
+  
+  const patchedIds = patchEmbeddingBatch(projectionWithIdArray);
+  console.log("patched", patchedIds);
 }
 
 main();
